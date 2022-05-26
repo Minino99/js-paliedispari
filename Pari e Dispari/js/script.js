e891441e989d169c1c02d4bb75661ad73d86b1f9
Minino99 <loizzo.pasqualino@gmail.com>
2022-05-26 16:53:30 +0200

    esercizi completi

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -8,7 +8,42 @@
 // ha vinto l’utente, altrimenti il computer
 
 let pickInput = prompt("pari o dispari?");
+let userWinner
 
-while (pickInput.toUpperCase() !== "PARI" || pickInput.toUpperCase() !== "DISPARI"){
+while (pickInput.toUpperCase() !== "PARI" && pickInput.toUpperCase() !== "DISPARI"){
   pickInput = prompt("hai inserito un valore non valido, scegli, pari o dispari?");
+}
+
+let numberInput = parseInt(prompt("scegli un numero da 1 a 5"));
+
+while (numberInput > 5 || numberInput < 1){
+  numberInput = parseInt(prompt("hai inserito un valore non valido, scegli un numero da 1 a 5"));
+}
+
+function randomRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+let cpuPick = randomRange(1, 5);
+
+console.log(cpuPick);
+
+let cpuUserSum = cpuPick + numberInput;
+
+function even(number){
+  if (number % 2 === 1){
+    return false
+  }
+  return true
+}
+
+if (pickInput.toUpperCase() === "PARI" && even(cpuUserSum)){
+  userWinner = alert(`La CPU ha scelto il numero ${cpuPick} e la somma tra i vostri numeri e' ${cpuUserSum} pertanto 
+  HAI VINTO!`);
+}else if (pickInput.toUpperCase() === "DISPARI"){
+  userWinner = alert(`La CPU ha scelto il numero ${cpuPick} e la somma tra i vostri numeri e' ${cpuUserSum} pertanto 
+  HAI VINTO!`);
+}else{
+  userWinner = alert(`La CPU ha scelto il numero ${cpuPick} e la somma tra i vostri numeri e' ${cpuUserSum} pertanto 
+  HAI PERSO!`);
 }
